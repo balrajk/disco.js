04863f2aa55bdaa6e124da24b141a73957acc218
Joe Mathai <joemathai16@gmail.com>
2014-02-17 09:04:06 -0500

    adding BBP algoithm

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Disco.js' });
 };
+exports.pi= function(req,res){
+    res.render('Pi',{ title: 'Pi generator'});
+}
